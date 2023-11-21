4f3167d54e08fc92cd24d7a5c8da9c1b64cb29d4
GenjiruSUchiwa <genjiru23@gmail.com>
2023-11-21 16:29:06 +0100

    tests: Writing tests for Entity. Related to #17

---
+++ backend/Place.Api/src/Place.Api.Tests.Domain/Common/EntityTests.cs
@@ -0,0 +1,37 @@
+namespace Place.Api.Tests.Domain.Common;
+
+using Api.Domain.Common.Abstractions;
+using FluentAssertions;
+using NSubstitute;
+
+public class EntityTests
+{
+    [Fact]
+    public void EqualsShouldReturnTrueWhenComparingTwoEqualEntities()
+    {
+        // Arrange
+        Guid entityId = Guid.NewGuid();
+        Entity<Guid>? entity1 = Substitute.For<Entity<Guid>>();
+        Entity<Guid>? entity2 = Substitute.For<Entity<Guid>>();
+
+        // Act
+        bool result = entity1.Equals(entity2);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void EqualsShouldReturnFalseWhenComparingTwoDifferentEntities()
+    {
+        // Arrange
+        Entity<int>? entity1 = Substitute.For<Entity<int>>();
+        Entity<Guid>? entity2 = Substitute.For<Entity<Guid>>();
+
+        // Act
+        bool result = entity1.Equals(entity2);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
--- backend/Place.Api/src/Place.Api.Tests.Domain/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Place.Api.Tests.Domain;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
