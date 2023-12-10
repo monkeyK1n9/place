bde26ef1596606b8445fa9c91d4c445dc4a3a5b4
GenjiruSUchiwa <genjiru23@gmail.com>
2023-12-10 15:45:39 +0100

    feat: Add Migrations

---
+++ backend/Place.Api/src/Place.Api.Infrastructure/Persistence/Migrations/20231210075254_Initial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Place.Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "user");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "user",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(26)", nullable: false),
+                    UserName = table.Column<string>(type: "character varying(12)", maxLength: 12, nullable: false),
+                    Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    LastName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    EmailIsConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    CreatedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedOnUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Email",
+                schema: "user",
+                table: "users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_EmailIsConfirmed",
+                schema: "user",
+                table: "users",
+                column: "EmailIsConfirmed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "user");
+        }
+    }
+}
