7742fb01b8749454710a672026d7b16893b45d90
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-22 11:26:13 +0100

    change to collection initialization

--- backend/Place.Api/src/Place.Api.Presentation/Endpoints/ApiRoutes.cs
+++ backend/Place.Api/src/Place.Api.Presentation/Endpoints/ApiRoutes.cs
@@ -8,7 +8,7 @@ public static class ApiRoutes
         public const string Endpoint = "regsiter";
         public const string Summary = "Registers a new user";
         public const string OperationId = "RegisterUser";
-        public static readonly string[] Tags = ["Authentication", "Register"];
+        public static readonly string[] Tags = { "Authentication", "Register" };
         public const string Description = "Allows users to create a new account by providing their registration details. Upon successful registration, the system will generate unique credentials, and the user will gain access to the platform's features";
         public const string SuccessMessage = "User created successfuly";
     }
@@ -27,7 +27,7 @@ public static class ApiRoutes
         public const string Endpoint = "forgotpassword";
         public const string Summary = "Recovers a user's account";
         public const string OperationId = "RecoverUser";
-        public static readonly string[] Tags = ["Authentication", "ForgotPassword"];
+        public static readonly string[] Tags = { "Authentication", "ForgotPassword" };
         public const string Description = "Allows users to recover their account by sending a one-time password reset (OTP) by mail and validating the reset password";
         public const string SuccessMessage = "OTP sent successfully";
     }
@@ -37,7 +37,7 @@ public static class ApiRoutes
         public const string Endpoint = "verifyotp";
         public const string Summary = "Recovers a user's account";
         public const string OperationId = "RecoverUser";
-        public static readonly string[] Tags = ["Authentication", "VerifyOTP"];
+        public static readonly string[] Tags = { "Authentication", "VerifyOTP" };
         public const string Description = "Allows users to verify the otp sent by mail inorder to permit him/her to change the password.";
         public const string SuccessMessage = "OTP verified successfully";
     }
@@ -47,7 +47,7 @@ public static class ApiRoutes
         public const string Endpoint = "resetpassword";
         public const string Summary = "Change a user's password after otp verification";
         public const string OperationId = "RecoverUser";
-        public static readonly string[] Tags = ["Authentication", "ResetPassword"];
+        public static readonly string[] Tags = { "Authentication", "ResetPassword" };
         public const string Description = "Allows users to change their password after forgetting their password and verifying the otp.";
         public const string SuccessMessage = "New password saved successfully";
     }
